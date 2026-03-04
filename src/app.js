2fd3b3ed20cd6191568a508ad32d2723085fef21
Pariskrit12 <bhagataryan784@gmail.com>
2026-03-04 17:32:11 +0545

    setup: configure Express app with CORS, body parsing, and cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
 
-const app=express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+app.use(
+  express.json({
+    limit: "20kb",
+  }),
+);
+app.use(
+  express.urlencoded({
+    limit: "20kb",
+  }),
+);
+app.use(cookieParser());
 
 export default app;
