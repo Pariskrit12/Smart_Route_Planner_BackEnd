d8bd56d6e41a6eeabf12d67121d10247fd0b42fb
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-03-10 21:09:39 +0545

    Created a middleware for authorizing protected route

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { User } from "../models/user_model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+const authorization = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized access");
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decoded) {
+      throw new ApiError(400, "Invalid Token");
+    }
+
+    const user = await User.findById(decoded._id).select(
+      "-password -refreshToken",
+    );
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(500, "Invalid access token", error);
+  }
+});
+export { authorization };
