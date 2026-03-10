7f658810e4515b87cbac83909b5f336d32ddf92d
Pariskrit Bhagat <bhagataryan784@gmail.com>
2026-03-10 23:47:49 +0545

    Add route for user logout and refresh token and userprofile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { userLogin, userRegister } from "../controllers/user.controller.js";
+import {
+  refreshToken,
+  userLogin,
+  userLogout,
+  userProfile,
+  userRegister,
+} from "../controllers/user.controller.js";
+import { authorization } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegister);
 router.route("/login").post(userLogin);
+router.route("/refreshToken").get(refreshToken);
+//protected routes
+router.route("/logout").post(authorization, userLogout);
+router.route("/profile").get(authorization, userProfile);
 export default router;
